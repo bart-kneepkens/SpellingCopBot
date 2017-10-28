85bddd04d63b8dc30082823c9e9e9342c2d70a47
Bart <bart-kneepkens@users.noreply.github.com>
2017-10-28 19:02:28 +0200

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,14 +5,9 @@ let token = readToken(from: "HELLO_BOT_TOKEN")
 let bot = TelegramBot(token: token)
 
 while let update = bot.nextUpdateSync() {
-    print("Received something")
-    if let message = update.message, let from = message.from, let text = message.text {
-        print(message.chat)
-        print(text)
-        
+    if let message = update.message, let text = message.text {
         if text.lowercased().replacingOccurrences(of:" ", with:"").contains("maiko") {
-            bot.sendMessageAsync(chat_id: message.chat.id,
-                                 text: "Miako*")
+            bot.sendMessageAsync(chat_id: message.chat.id, text: "Miako*")
         }
     }
 }
