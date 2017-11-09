bd9258e43ef4f81987beb5deec8a4d57ea1235cf
Bart Kneepkens <bkneepkens@msn.com>
2017-11-09 23:18:30 +0100

    File skeleton for string extensions.

---
+++ Sources/String+Extras.swift
@@ -0,0 +1,18 @@
+//
+//  String+Extras.swift
+//  miakoBot
+//
+//  Created by Bart Kneepkens on 09/11/2017.
+//
+
+import Foundation
+
+extension String {
+    var isUsernameTag: Bool {
+        return true
+    }
+    
+    var isUrl: Bool {
+        return true
+    }
+}
