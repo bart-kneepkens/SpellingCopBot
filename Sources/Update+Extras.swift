823ecdfb3f9f739ca026a191f1bcef835e38c0a5
Bart Kneepkens <bkneepkens@msn.com>
2017-11-12 23:17:34 +0100

    Fix command matching for a working linux build.

--- Sources/Update+Extras.swift
+++ Sources/Update+Extras.swift
@@ -11,14 +11,11 @@ import TelegramBot
 extension Update {
     var containsCommand: Bool {
         guard let text = self.message?.text else { return false }
-        guard text.starts(with: "/") else { return false }
+        guard text.hasPrefix("/") else { return false }
         let messageComponents = text
             .trimmed(set: CharacterSet.alphanumerics.inverted)
             .components(separatedBy: .whitespaces)
-        guard var firstComponent = messageComponents.first else { return false }
-        if let atSignIndex = firstComponent.index(of: "@") {
-            firstComponent = firstComponent.substring(to: atSignIndex)
-        }
-        return CommandName.all.map({$0.rawValue}).contains(firstComponent)
+        guard let firstComponent = messageComponents.first else { return false }
+        return CommandName.all.map({$0.rawValue}).contains(where: { firstComponent.contains($0) })
     }
 }
