3a710ab782601d531152f5fe8b5a19d407fe7f10
Bart Kneepkens <bkneepkens@msn.com>
2018-01-24 21:10:27 +0100

    Trying desperately to keep the bot alive without patching Swift 3

--- Sources/miakoBot/main.swift
+++ Sources/miakoBot/main.swift
@@ -9,9 +9,12 @@ typealias Chat = Int64
 // Dirty, dirty hack. telegram-bot-swift has an issue which makes the app un-responding after a while of inactivity.
 // This way, it will stay alive.
 let timer = DispatchSource.makeTimerSource()
+var do_not_touch: Bool = false
 fileprivate func keepMeAlive() {
-    timer.scheduleRepeating(deadline: .now(), interval: .seconds(1))
-    timer.setEventHandler(handler: {})
+    timer.scheduleRepeating(deadline: .now(), interval: .milliseconds(10))
+    timer.setEventHandler(handler: {
+        do_not_touch = !do_not_touch
+    })
     timer.resume()
 }
 
